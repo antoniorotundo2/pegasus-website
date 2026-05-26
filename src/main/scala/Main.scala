22fa5e21d842193fa137d8c6d36162e31b4eec26
antoniorotundo2 <antonio.rotundo2@studio.unibo.it>
2026-05-26 21:53:21 +0200

    fix Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,12 +2,13 @@ import zio.http.*
 import zio.*
 
 object Main extends ZIOAppDefault:
-  val app: HttpApp[Any] = Http.collect[Request]:
-    case Method.GET -> Root =>
-      Response.text("Ciao da ZIO HTTP (Scala 3) su Render! 🚀")
-    case Method.GET -> Root / "saluta" / name =>
+  val routes = Routes(
+    Method.GET / Root -> handler(Response.text("Ciao da ZIO HTTP (Scala 3) su Render! 🚀")),
+    Method.GET / "saluta" / string("name") -> handler { (name: String) =>
       Response.text(s"Ciao, $name!")
+    }
+  )
 
-  val server = Server.serve(app).provide(Server.defaultWithPort(8080))
+  val server = Server.serve(routes).provide(Server.defaultWithPort(8080))
 
   def run = server
